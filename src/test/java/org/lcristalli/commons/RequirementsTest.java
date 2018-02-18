c15c027f45b0b4354d77e048242921d7859cb090
Luigi Cristalli <luigi.cristalli@gmail.com>
2018-02-18 21:00:02 +0100

    Added a test to prove that composition of predicates does not have precedence

--- src/test/java/org/lcristalli/commons/RequirementsTest.java
+++ src/test/java/org/lcristalli/commons/RequirementsTest.java
@@ -3,13 +3,13 @@ package org.lcristalli.commons;
 import junitparams.JUnitParamsRunner;
 import junitparams.NamedParameters;
 import junitparams.Parameters;
-import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 
 import java.util.Objects;
+import java.util.function.Predicate;
 
 import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.junit.rules.ExpectedException.none;
@@ -41,7 +41,7 @@ public class RequirementsTest {
 
     @Test
     @Parameters(named = "testCasesForLogicalAnd")
-    public void whenUsingTwoPredicatesInAndWithOneOrBothPredicatesEvaluatingToFalseThenTheSuppliedExceptionIsThrown(String testedString) throws Exception {
+    public void whenUsingTwoPredicatesInAndWithOneOrBothPredicatesEvaluatingToFalseThenTheSuppliedExceptionIsThrown(String testedString) {
         final RuntimeException thrownException = new RuntimeException("test");
 
         expectedException.expect(sameInstance(thrownException));
@@ -59,7 +59,7 @@ public class RequirementsTest {
     }
 
     @Test
-    public void whenCombiningWithLogicalAndTwoPredicatesEvaluatingToTrueThenNoExceptionIsThrown() throws Exception {
+    public void whenCombiningWithLogicalAndTwoPredicatesEvaluatingToTrueThenNoExceptionIsThrown() {
         final String testedString = "passingTest";
 
         require(testedString).toSatisfy(s -> s.startsWith("p")).and("passingTest"::equals).otherwiseThrow(RuntimeException::new);
@@ -67,7 +67,7 @@ public class RequirementsTest {
 
     @Test
     @Parameters(named = "testCasesForLogicalOr")
-    public void whenUsingTwoPredicatesInOrWithOneOrBothPredicatesEvaluatingToTrueThenNoExceptionIsThrown(String testedString) throws Exception {
+    public void whenUsingTwoPredicatesInOrWithOneOrBothPredicatesEvaluatingToTrueThenNoExceptionIsThrown(String testedString) {
         require(testedString).toSatisfy(s -> s.startsWith("a")).or(s -> s.endsWith("z")).otherwiseThrow(RuntimeException::new);
     }
 
@@ -89,8 +89,32 @@ public class RequirementsTest {
         require("test").toSatisfy(s -> s.startsWith("a")).or(s -> s.startsWith("b")).otherwiseThrow(() -> thrownException);
     }
 
+    /**
+     * This test proves that when building the {@link Predicate} there is no precedence of the {@code and} over the {@code or}.
+     * The test is made by testing the following composition of {@link Predicate}s
+     *
+     * {@code A or B and C}
+     *
+     * in which:
+     * - {@code A} is {@code true}
+     * - {@code B} is {@code false}
+     * - {@code C} is {@code false}
+     *
+     * With the precedence this evaluates {@code A or (B and C) == true}.
+     * Without the precedence this evaluates {@code (A or B) and C == false}
+     *
+     * Therefore we expect an exception
+     */
     @Test
-    public void logicalConnectorsAreAppliedInTheOrderTheyAreUsed() throws Exception {
-        Assert.fail();
+    public void logicalConnectorsAreAppliedInTheOrderTheyAreUsed() {
+        final String testedString = "test";
+
+        expectedException.expect(RuntimeException.class);
+
+        require(testedString)
+                .toSatisfy(s -> s.startsWith("t"))
+                .or(s -> s.startsWith("a"))
+                .and(s -> s.startsWith("b"))
+                .otherwiseThrow(RuntimeException::new);
     }
 }
